7ee39851a65838475c4ea4fc20c0d5a494478938
Eloise (MarchBig) <eloiseha@gmail.com>
2025-06-13 18:59:05 +0800

    Updated Recovery

--- assets/js/pages/form-editor.init.js
+++ assets/js/pages/form-editor.init.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("#summernote-editor").summernote({height:250,minHeight:null,maxHeight:null,focus:!1}),$("#summernote-inline").summernote({airMode:!0})});
+$(document).ready(function(){$("#summernote-editor").summernote({height:250,minHeight:null,maxHeight:null,focus:!1}),$("#summernote-inline").summernote({airMode:!0}),$(".summernote-editor").summernote({height:250,minHeight:null,maxHeight:null,focus:!1})});
--- src/js/pages/form-editor.init.js
+++ src/js/pages/form-editor.init.js
@@ -17,4 +17,10 @@ $(document).ready(function(){
     $('#summernote-inline').summernote({
         airMode: true
     });
+    $('.summernote-editor').summernote({
+        height: 250,                 // set editor height
+        minHeight: null,             // set minimum height of editor
+        maxHeight: null,             // set maximum height of editor
+        focus: false                 // set focus to editable area after initializing summernote
+    });
 });
